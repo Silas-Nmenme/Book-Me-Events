408d34de9ee45275a1235acc9baa8f3ba462fe9f
Your Name <you@example.com>
2026-05-08 09:13:09 +0100

    `Removed unused middleware and routes, updated API response message`

--- app.js
+++ app.js
@@ -1,52 +1,12 @@
-require('dotenv').config();
-
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-
-const { errorHandler } = require('./src/middlewares/errorMiddleware');
-
-const authRoutes = require('./src/routes/authRoutes');
-const userRoutes = require('./src/routes/userRoutes');
-const vendorRoutes = require('./src/routes/vendorRoutes');
-const serviceRoutes = require('./src/routes/serviceRoutes');
-const requestRoutes = require('./src/routes/requestRoutes');
-const bookingRoutes = require('./src/routes/bookingRoutes');
-const paymentRoutes = require('./src/routes/paymentRoutes');
-const reviewRoutes = require('./src/routes/reviewRoutes');
-const messageRoutes = require('./src/routes/messageRoutes');
-const adminRoutes = require('./src/routes/adminRoutes');
 
 const app = express();
-
-app.use(cors());
-app.use(express.json({ limit: '15mb' }));
-app.use(express.urlencoded({ extended: true, limit: '15mb' }));
-app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({
     success: true,
-    message: 'Book Me Events API is running'
+    message: 'Server Working'
   });
 });
-
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/vendors', vendorRoutes);
-app.use('/api/v1/services', serviceRoutes);
-app.use('/api/v1/requests', requestRoutes);
-app.use('/api/v1/bookings', bookingRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-app.use('/api/v1/reviews', reviewRoutes);
-app.use('/api/v1/messages', messageRoutes);
-app.use('/api/v1/admin', adminRoutes);
-
-app.use((req, res, next) => {
-  res.status(404);
-  next(new Error(`Not Found - ${req.originalUrl}`));
-});
-
-app.use(errorHandler);
 
 module.exports = app;
